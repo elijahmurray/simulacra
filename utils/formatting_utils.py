2c91679e975507a26005d6d528f9b86e763ad347
Elijah Murray <3240306+elijahmurray@users.noreply.github.com>
2023-04-29 09:52:48 -0700

    add logging, openai, and formatting utils from Elijahs branch

---
+++ utils/formatting_utils.py
@@ -0,0 +1,18 @@
+def datetime_formatter(datetime):
+    return round_time_to_nearest_5_minutes(datetime).strftime("%A, %B %d, %Y %I:%M%p")
+
+
+def date_formatter(datetime):
+    return datetime.strftime("%B %d, %Y")
+
+
+def hour_formatter(datetime):
+    return datetime.strftime("%I:%M %p")
+
+
+def round_time_to_nearest_5_minutes(datetime):
+    minute = 5 * round(datetime.minute / 5)
+    if minute >= 60:
+        minute = 0
+        datetime = datetime.replace(hour=datetime.hour + 1)
+    return datetime.replace(second=0, microsecond=0, minute=minute)
--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,2 +1,48 @@
+from colorama import Fore, Style
+import inspect
+from config import (
+    ENABLE_METHOD_CALLED_LOGGING,
+    ENABLE_PROMPT_LOGGING,
+    ENABLE_PROMPT_CONTEXT_LOGGING,
+    ENABLE_AGENT_EVENT_LOGGING,
+    ENABLE_WORLD_EVENT_LOGGING,
+    ENABLE_LLM_RESPONSE_LOGGING,
+)
+
+
 class Logger:
-    pass
+    def log(message, type, force_log=False):
+        if force_log:
+            print(f"{Fore.RED}{message}{Style.RESET_ALL}")
+            return
+        if type == "method" and ENABLE_METHOD_CALLED_LOGGING:
+            print(
+                f"{Fore.RED}\n==========================\nCALLED: {message}\n=========================={Style.RESET_ALL}"
+            )
+            return
+        if type == "prompt" and ENABLE_PROMPT_LOGGING:
+            print(f"{Fore.MAGENTA}{message}{Style.RESET_ALL}")
+            return
+        if type == "context" and ENABLE_PROMPT_CONTEXT_LOGGING:
+            print(f"{Fore.RED}{message}{Style.RESET_ALL}")
+            return
+        if type == "agent_event" and ENABLE_AGENT_EVENT_LOGGING:
+            print(f"{Fore.WHITE}{message}{Style.RESET_ALL}")
+            return
+        if type == "world_event" and ENABLE_WORLD_EVENT_LOGGING:
+            print(f"{Fore.YELLOW}{message}{Style.RESET_ALL}")
+            return
+        if type == "llm_response" and ENABLE_LLM_RESPONSE_LOGGING:
+            print(f"{Fore.GREEN}{message}{Style.RESET_ALL}")
+            return
+        else:
+            return
+
+    def calling_method_name():
+        # get the frame object of the calling function
+        frame = inspect.currentframe().f_back
+        # get the code object of the calling function
+        code = frame.f_code
+        # get the name of the calling function
+        name = code.co_name
+        return name
